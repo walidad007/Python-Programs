df2d4859d6f687d63bd06de99b6d4d6e33b5ed36
walidad007 <walidadmahtam62@gmail.com>
2024-02-25 18:52:03 +0500

    String Cancatenation

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# string concatenation (how to put string together)
+
+adj = input("Adjective : ")
+verb1 = input("Verb1 : ")
+verb2 = input("Verb2 : ")
+famous_person = input("Famous Person : ")
+
+# concatenate all the above input strings togetter.
+madlibs = f"cmputer Programming is so {adj}! it makes me so excited all the time \
+because i love to {verb1}. Stay hudrated and {verb2} like your are {famous_person}!"
+
+print(madlibs)
