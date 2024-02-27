9eeb21df29a9ce9346822ff23d01f8e33953d7a1
walidad007 <walidadmahtam62@gmail.com>
2024-02-27 20:27:05 +0500

    Computer_Guess

--- Guess_Number.py
+++ Guess_Number.py
@@ -32,6 +32,23 @@ def guess(x):
     # If the user's guess matches the random_number, 
     # the loop terminates, and the following message is printed:
     print(f'Yay, Congrats. You have Guessed the Number {random_number} correctly!! ')
-    
-guess(20)
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback= '' 
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low # could also high b/c low=high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print('Yay! The computer gueesed your number, {guess}, correctly!')       
+               
+computer_guess(1000)
             
